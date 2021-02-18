63de5bc68b93a547c50346ffa84e5bd9160c67b2
Boris Brenner <boris.brenner@netmedia.de>
2021-02-18 14:15:18 +0100

    changed image urls

--- documentation/src/data/customer-data.js
+++ documentation/src/data/customer-data.js
@@ -5,7 +5,7 @@ export default [
       'We had this idea to teach how to use Microsoft 365 in a playful way.<br>We started with a PDF version of the Treasure Hunt.<br><br>But this solution gives us a much more engaging user experience.',
     customerName: 'Viktoria Strobl',
     customerTitle: 'Project Treasure Hunt, Microsoft',
-    customerImage: 'https://placeimg.com/150/150/people'
+    customerImage: 'https://media-exp1.licdn.com/dms/image/C4D03AQGFS8Z6pl_OyA/profile-displayphoto-shrink_800_800/0/1599940056309?e=1619049600&v=beta&t=uDJ6GVGgnwHVWfJFPfBWF6faoTpx4yRS0YJY_S3fe94'
   },
   {
     title: 'The power of Microsoft 365',
@@ -13,7 +13,7 @@ export default [
       'Treasure Hunt is a great example of the power and flexibility of M365 features when combined.<br>Microsoft Teams, Power Apps and Microsoft Lists merge into a game-changing learning solution<br>for everyone.',
     customerName: 'Judith Schmitz',
     customerTitle: 'Microsoft',
-    customerImage: 'https://placeimg.com/150/150/people'
+    customerImage: 'https://media-exp1.licdn.com/dms/image/C4D03AQHET1maN6GViw/profile-displayphoto-shrink_400_400/0/1537168118345?e=1619049600&v=beta&t=JlOrX32ojgH69SnhZNKnWMUq8urpGEqXh2HSTwa2F3M'
   },
   {
     title: 'Business values meet fun learning',
@@ -21,6 +21,6 @@ export default [
       'Having fun supports the learning process.<br><br>When developing the story around the "Treasure Hunt Pirates", we placed particular emphasis not only on the textual elaboration of the story, but also on the visual language and dramaturgy.',
     customerName: 'Sorina Becheru',
     customerTitle: 'Senior Consultant, netmedia',
-    customerImage: 'https://placeimg.com/150/150/people'
+    customerImage: 'https://media-exp1.licdn.com/dms/image/C5603AQFtJRwR3HgCUQ/profile-displayphoto-shrink_400_400/0/1584353365647?e=1619049600&v=beta&t=wR-lXc2MhkGRQL6GPkKcUSiSmMOGz6gRcDpOXjHMuy4'
   }
 ];
