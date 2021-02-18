74a66da7f349c4ed219e548a76f38ab91dd71d42
Boris Brenner <boris.brenner@netmedia.de>
2021-02-18 14:38:55 +0100

    changed customer card template

--- documentation/src/components/CustomerCard.js
+++ documentation/src/components/CustomerCard.js
@@ -13,7 +13,7 @@ const CustomerCard = ({ customer }) => (
       />
       <div>
         <p>{customer.customerName}</p>
-        <p className="text-sm text-gray-600">{customer.customerTitle}</p>
+        <p className="text-sm text-gray-600" dangerouslySetInnerHTML={{__html: customer.customerTitle}}></p>
       </div>
     </div>
   </Card>
