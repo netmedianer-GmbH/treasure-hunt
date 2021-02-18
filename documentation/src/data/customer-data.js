f619a90da8e6e8abc115c5d769f8d4399d3def0f
Boris Brenner <boris.brenner@netmedia.de>
2021-02-18 14:33:51 +0100

    updated customerTitle

--- documentation/src/data/customer-data.js
+++ documentation/src/data/customer-data.js
@@ -4,7 +4,7 @@ export default [
     content:
       'We had this idea to teach how to use Microsoft 365 in a playful way.<br>We started with a PDF version of the Treasure Hunt.<br><br>But this solution gives us a much more engaging user experience.',
     customerName: 'Viktoria Strobl',
-    customerTitle: 'Project Treasure Hunt, Microsoft',
+    customerTitle: 'Project Treasure Hunt<br>Microsoft',
     customerImage: 'https://media-exp1.licdn.com/dms/image/C4D03AQGFS8Z6pl_OyA/profile-displayphoto-shrink_800_800/0/1599940056309?e=1619049600&v=beta&t=uDJ6GVGgnwHVWfJFPfBWF6faoTpx4yRS0YJY_S3fe94'
   },
   {
@@ -12,7 +12,7 @@ export default [
     content:
       'Treasure Hunt is a great example of the power and flexibility of M365 features when combined.<br>Microsoft Teams, Power Apps and Microsoft Lists merge into a game-changing learning solution<br>for everyone.',
     customerName: 'Judith Thea Schmitz',
-    customerTitle: 'Customer Success Manager, Microsoft',
+    customerTitle: 'Customer Success Manager<br>Microsoft',
     customerImage: 'https://media-exp1.licdn.com/dms/image/C4D03AQHET1maN6GViw/profile-displayphoto-shrink_400_400/0/1537168118345?e=1619049600&v=beta&t=JlOrX32ojgH69SnhZNKnWMUq8urpGEqXh2HSTwa2F3M'
   },
   {
@@ -20,7 +20,7 @@ export default [
     content:
       'Having fun supports the learning process.<br><br>When developing the story around the "Treasure Hunt Pirates", we placed particular emphasis not only on the textual elaboration of the story, but also on the visual language and dramaturgy.',
     customerName: 'Sorina Becheru',
-    customerTitle: 'Senior Consultant, netmedianer GmbH',
+    customerTitle: 'Senior Consultant<br>netmedianer GmbH',
     customerImage: 'https://media-exp1.licdn.com/dms/image/C5603AQFtJRwR3HgCUQ/profile-displayphoto-shrink_400_400/0/1584353365647?e=1619049600&v=beta&t=wR-lXc2MhkGRQL6GPkKcUSiSmMOGz6gRcDpOXjHMuy4'
   }
 ];
