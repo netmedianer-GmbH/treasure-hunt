3584cde3259334ec373a4b53c229407d18fe948c
Boris Brenner <boris.brenner@netmedia.de>
2021-02-23 16:39:04 +0100

    fixed profile pic url path

--- documentation/src/data/customer-data.js
+++ documentation/src/data/customer-data.js
@@ -4,7 +4,7 @@ export default [
     content: 'The initial idea was to teach how to use Microsoft 365 in a playful way. We started with a PDF version of the Treasure Hunt, where we combined all the different elements of a Gamification approach.<br>The vision grew fast and with this Power App solution, we can now engage the users even more.',
     customerName: 'Viktoria Bjarnhoff Strobl',
     customerTitle: 'Project Treasure Hunt<br>Microsoft',
-    customerImage: '/img/people/viktoria.jpeg'
+    customerImage: './img/people/viktoria.jpeg'
   },
   {
     title: 'The power of Microsoft 365',
@@ -12,7 +12,7 @@ export default [
       'Treasure Hunt is a great example of the power and flexibility of M365 features when combined.<br>Microsoft Teams, Power Apps and Microsoft Lists merge into a game-changing learning solution<br>for everyone.<br><br>',
     customerName: 'Judith Thea Schmitz',
     customerTitle: 'Customer Success Manager<br>Microsoft',
-    customerImage: '/img/people/judith.jpeg'
+    customerImage: './img/people/judith.jpeg'
   },
   {
     title: 'Business values meet fun learning',
@@ -20,6 +20,6 @@ export default [
       'Having fun supports the learning process.<br><br>When developing the story around the "Treasure Hunt Pirates", we placed particular emphasis not only on the textual elaboration of the story, but also on the visual language and dramaturgy.<br><br>',
     customerName: 'Sorina Becheru',
     customerTitle: 'Senior Consultant<br>netmedianer GmbH',
-    customerImage: '/img/people/sorina.jpeg'
+    customerImage: './img/people/sorina.jpeg'
   }
 ];
