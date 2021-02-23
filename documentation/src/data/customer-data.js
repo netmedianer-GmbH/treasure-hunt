dded858b96d6479b22825c4d6afc13a63871dda7
Boris Brenner <boris.brenner@netmedia.de>
2021-02-23 16:09:59 +0100

    changed content

--- documentation/src/data/customer-data.js
+++ documentation/src/data/customer-data.js
@@ -1,16 +1,15 @@
 export default [
   {
-    title: 'Learning by having fun',
-    content:
-      'We had this idea to teach how to use Microsoft 365 in a playful way.<br>We started with a PDF version of the Treasure Hunt.<br><br>But this solution gives us a much more engaging user experience.',
-    customerName: 'Viktoria Strobl',
+    title: 'The Gamification Element',
+    content: 'The initial idea was to teach how to use Microsoft 365 in a playful way. We started with a PDF version of the Treasure Hunt, where we combined all the different elements of a Gamification approach.<br>The vision grew fast and with this Power App solution, we can now engage the users even more.',
+    customerName: 'Viktoria Bjarnhoff Strobl',
     customerTitle: 'Project Treasure Hunt<br>Microsoft',
     customerImage: 'https://media-exp1.licdn.com/dms/image/C4D03AQGFS8Z6pl_OyA/profile-displayphoto-shrink_800_800/0/1599940056309?e=1619049600&v=beta&t=uDJ6GVGgnwHVWfJFPfBWF6faoTpx4yRS0YJY_S3fe94'
   },
   {
     title: 'The power of Microsoft 365',
     content:
-      'Treasure Hunt is a great example of the power and flexibility of M365 features when combined.<br>Microsoft Teams, Power Apps and Microsoft Lists merge into a game-changing learning solution<br>for everyone.',
+      'Treasure Hunt is a great example of the power and flexibility of M365 features when combined.<br>Microsoft Teams, Power Apps and Microsoft Lists merge into a game-changing learning solution<br>for everyone.<br><br>',
     customerName: 'Judith Thea Schmitz',
     customerTitle: 'Customer Success Manager<br>Microsoft',
     customerImage: 'https://media-exp1.licdn.com/dms/image/C4D03AQHET1maN6GViw/profile-displayphoto-shrink_400_400/0/1537168118345?e=1619049600&v=beta&t=JlOrX32ojgH69SnhZNKnWMUq8urpGEqXh2HSTwa2F3M'
@@ -18,7 +17,7 @@ export default [
   {
     title: 'Business values meet fun learning',
     content:
-      'Having fun supports the learning process.<br><br>When developing the story around the "Treasure Hunt Pirates", we placed particular emphasis not only on the textual elaboration of the story, but also on the visual language and dramaturgy.',
+      'Having fun supports the learning process.<br><br>When developing the story around the "Treasure Hunt Pirates", we placed particular emphasis not only on the textual elaboration of the story, but also on the visual language and dramaturgy.<br><br>',
     customerName: 'Sorina Becheru',
     customerTitle: 'Senior Consultant<br>netmedianer GmbH',
     customerImage: 'https://media-exp1.licdn.com/dms/image/C5603AQFtJRwR3HgCUQ/profile-displayphoto-shrink_400_400/0/1584353365647?e=1619049600&v=beta&t=wR-lXc2MhkGRQL6GPkKcUSiSmMOGz6gRcDpOXjHMuy4'
