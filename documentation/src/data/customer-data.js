2100473e5080df486272719273385244e5afe2e7
Boris Brenner <boris.brenner@netmedia.de>
2021-02-23 16:33:12 +0100

    added user profile pics

--- documentation/src/data/customer-data.js
+++ documentation/src/data/customer-data.js
@@ -4,7 +4,7 @@ export default [
     content: 'The initial idea was to teach how to use Microsoft 365 in a playful way. We started with a PDF version of the Treasure Hunt, where we combined all the different elements of a Gamification approach.<br>The vision grew fast and with this Power App solution, we can now engage the users even more.',
     customerName: 'Viktoria Bjarnhoff Strobl',
     customerTitle: 'Project Treasure Hunt<br>Microsoft',
-    customerImage: 'https://media-exp1.licdn.com/dms/image/C4D03AQGFS8Z6pl_OyA/profile-displayphoto-shrink_800_800/0/1599940056309?e=1619049600&v=beta&t=uDJ6GVGgnwHVWfJFPfBWF6faoTpx4yRS0YJY_S3fe94'
+    customerImage: '/img/people/viktoria.jpeg'
   },
   {
     title: 'The power of Microsoft 365',
@@ -12,7 +12,7 @@ export default [
       'Treasure Hunt is a great example of the power and flexibility of M365 features when combined.<br>Microsoft Teams, Power Apps and Microsoft Lists merge into a game-changing learning solution<br>for everyone.<br><br>',
     customerName: 'Judith Thea Schmitz',
     customerTitle: 'Customer Success Manager<br>Microsoft',
-    customerImage: 'https://media-exp1.licdn.com/dms/image/C4D03AQHET1maN6GViw/profile-displayphoto-shrink_400_400/0/1537168118345?e=1619049600&v=beta&t=JlOrX32ojgH69SnhZNKnWMUq8urpGEqXh2HSTwa2F3M'
+    customerImage: '/img/people/judith.jpeg'
   },
   {
     title: 'Business values meet fun learning',
@@ -20,6 +20,6 @@ export default [
       'Having fun supports the learning process.<br><br>When developing the story around the "Treasure Hunt Pirates", we placed particular emphasis not only on the textual elaboration of the story, but also on the visual language and dramaturgy.<br><br>',
     customerName: 'Sorina Becheru',
     customerTitle: 'Senior Consultant<br>netmedianer GmbH',
-    customerImage: 'https://media-exp1.licdn.com/dms/image/C5603AQFtJRwR3HgCUQ/profile-displayphoto-shrink_400_400/0/1584353365647?e=1619049600&v=beta&t=wR-lXc2MhkGRQL6GPkKcUSiSmMOGz6gRcDpOXjHMuy4'
+    customerImage: '/img/people/sorina.jpeg'
   }
 ];
