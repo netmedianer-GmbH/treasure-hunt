84840e361bd5f0b50b6996e24b0b64c54ba39809
Boris Brenner <boris.brenner@netmedia.de>
2021-02-23 16:53:51 +0100

    changed footer

--- documentation/src/components/layout/Footer.js
+++ documentation/src/components/layout/Footer.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Footer = () => (
   <footer className="container mx-auto py-16 px-3 mt-48 mb-8 text-gray-800">
     <div className="flex -mx-3">
-      <div className="flex-1 px-3">
+      {/* <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">About Us</h2>
         <p className="mt-5">Ridiculus mus mauris vitae ultricies leo integer malesuada nunc.</p>
-      </div>
-      <div className="flex-1 px-3">
+      </div> */}
+      {/* <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">Important Links</h2>
         <ul className="mt-4 leading-loose">
           <li>
@@ -17,8 +17,8 @@ const Footer = () => (
             <a href="https://codebushi.com">Privacy Policy</a>
           </li>
         </ul>
-      </div>
-      <div className="flex-1 px-3">
+      </div> */}
+      {/* <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">Social Media</h2>
         <ul className="mt-4 leading-loose">
           <li>
@@ -31,7 +31,7 @@ const Footer = () => (
             <a href="https://github.com/codebushi/gatsby-starter-lander">GitHub</a>
           </li>
         </ul>
-      </div>
+      </div> */}
     </div>
   </footer>
 );
