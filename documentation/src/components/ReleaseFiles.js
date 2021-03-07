b2f44437f4f4f1d266e1e3da1b2e090cdd054a87
Boris Brenner <boris.brenner@netmedia.de>
2021-03-07 12:40:26 +0100

    added ul/li to the file list

--- documentation/src/components/ReleaseFiles.js
+++ documentation/src/components/ReleaseFiles.js
@@ -17,18 +17,24 @@ const ReleaseFiles = () => {
     return (
         <div className="pt-12 px-10 lg:px-24 text-left">
             <h2 className="text-2xl my-4">Download the latest release files from here</h2>
+
             <h3 className="text-xl my-4">The Microsoft Excel Files</h3>
+            <ul className="list-disc text-lg pl-6 py-4">
             {
                 releases[0]?.assets.map(asset =>
                     asset.content_type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                    && <div><a href={asset.browser_download_url}>{asset.name}</a></div>)
+                    && <li><a href={asset.browser_download_url}>{asset.name}</a></li>)
             }
+            </ul>
+
             <h3 className="text-xl my-4">The PowerApps Canvas App Import File</h3>
+            <ul className="list-disc text-lg pl-6 py-4">
             {
                 releases[0]?.assets.map(asset =>
                     asset.content_type === "application/zip"
-                    && <div><a href={asset.browser_download_url}>{asset.name}</a></div>)
+                    && <li><a href={asset.browser_download_url}>{asset.name}</a></li>)
             }
+            </ul>
         </div>
     )
 };
