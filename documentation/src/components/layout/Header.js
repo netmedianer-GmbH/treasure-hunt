f6175443d77848dc5798c6dd01c2b070133ef14e
Boris Brenner <boris.brenner@netmedia.de>
2021-03-08 09:40:39 +0100

    fixed img path

--- documentation/src/components/layout/Header.js
+++ documentation/src/components/layout/Header.js
@@ -65,7 +65,7 @@ const Header = (props) => {
         <div className="hidden md:block">
           <Button size="small" className="text-sm" onClick={() => window.open("https://github.com/netmedianer-GmbH/treasure-hunt/", "_blank")}>
             <div className="flex justify-center items-center">
-            <img className="w-6" alt="github logo" src="/img/GitHub-Mark-Light-32px.png" />
+            <img className="w-6" alt="github logo" src="./img/GitHub-Mark-Light-32px.png" />
             <span className="pl-2">View on GitHub</span>
             </div>
           </Button>
